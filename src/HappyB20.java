35db25e2e9b2b9ea29356cf19081d0c5621026ea
cschmertz <cschmertz@gmail.com>
2020-08-01 12:05:23 -0400

    update two Batch20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,5 +2,6 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
         System.out.println("update");
+        System.out.println("update two");
     }
 }
